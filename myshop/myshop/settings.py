ae9665db7bfdccf49053e2135987194d1e0b7334
ruifengm <ruifengm@sg.ibm.com>
2018-07-01 15:32:57 +0800

    Added support for Heroku.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e%jlva@iq6yb3xzwr5hqdk2nj54$dw_nx3mk3v-l741h@r4qj-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'flyer-shop.herokuapp.com']
 
 
 # Application definition
@@ -114,8 +114,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
