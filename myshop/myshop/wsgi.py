c09202ef4c72f8adaf9be798cb8e789eaf69020f
ruifengm <ruifengm@sg.ibm.com>
2018-07-01 15:35:40 +0800

    Fixed wsgi.py

--- myshop/myshop/wsgi.py
+++ myshop/myshop/wsgi.py
@@ -13,7 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
